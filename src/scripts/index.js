70cb5129e7460a99ee679a2e7af1dbe59ce1fb7e
iciveas <zapahvesny@yandex.ru>
2023-03-25 19:57:30 +0400

    different forms opening

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,46 +1,74 @@
-let popup = document.querySelector('.popup');
-let form = document.querySelector('.form');
-
-let editProfileButton = document.querySelector('.user-panel__edit-button');
-let closeProfileButton = popup.querySelector('.popup__close-button');
-let saveProfileButton = form.querySelector('.form__save-button');
-console.dir(saveProfileButton);
-
-let profileName = document.querySelector('.user-panel__user-name');
-let profileSignature = document.querySelector('.user-panel__user-signature');
-
+//Создаем переменные для полей формы с инфо пользователя
 const nameInput = document.querySelector('.form__input_user-name');
 const signatureInput = document.querySelector('.form__input_user-signature');
 
-function openProfileEditor() {
-  popup.style.display = "";
-  nameInput.value = profileName.textContent;
-  signatureInput.value = profileSignature.textContent;
+//Создаем переменные для работы с новым постом
+const postTemplate = document.querySelector('#post-template').content;
+
+//Получаем поп-ап и все формы в нём
+const popup = document.querySelector('.popup');
+const profileEditForm = document.querySelector('[name="profile-info"]');
+const postAddForm = document.querySelector('[name="add-post"]');
+
+//Получаем текущие данные пользователя
+const profileName = document.querySelector('.user-panel__user-name');
+const profileSignature = document.querySelector('.user-panel__user-signature');
+
+//Получаем кнопки для вызова форм
+const editProfileButton = document.querySelector('.user-panel__edit-button');
+const addPostButton = document.querySelector('.user-panel__add-post-button');
+
+//Получаем кнопки, универсальные для форм
+const closePopupButton = popup.querySelector('.popup__close-button'); //кнопка закрытия формы
+const saveButton = document.querySelector('.form__save-button'); //кнопка 'отправки' данных из формы
+
+
+function openForm (evt) {
+  //Получаем текущую форму
+  const targetForm = evt.target;
 
   popup.classList.remove('popup_closed');
   popup.classList.add('popup_opened');
 
+  if (targetForm.className === 'user-panel__add-post-button') {
+    popup.style.display = "";
+    profileEditForm.style.display = "none";
+    postAddForm.style.display = "";
+  }
+  else {
+    popup.style.display = "";
+    postAddForm.style.display = "none";
+    profileEditForm.style.display = "";
+
+    //Заполняем поля текущими данными
+    nameInput.value = profileName.textContent;
+    signatureInput.value = profileSignature.textContent;
+  }
+
 }
 
-function closeProfileEditor() {
+function closeForm() {
   popup.classList.add('popup_closed');
   addEventListener('animationend', () => {popup.style.display = "none"; }, {once: true});
   popup.classList.remove('popup_opened');
-
 }
 
-function saveProfileInfo(evt) {
+function saveForm(evt) {
   evt.preventDefault();
 
-  profileName.textContent = nameInput.value;
-  profileSignature.textContent = signatureInput.value;
-
-  console.log('нажалось');
-  closeProfileEditor();
+  if (evt.target.name === 'add-post') {
+    createPost();
+  }
+  else {
+    profileName.textContent = nameInput.value;
+    profileSignature.textContent = signatureInput.value;
+  }
+  closeForm();
 }
 
-editProfileButton.addEventListener('click', openProfileEditor);
-closeProfileButton.addEventListener('click', closeProfileEditor);
-form.addEventListener('submit', saveProfileInfo);
+editProfileButton.addEventListener('click', openForm);
+addPostButton.addEventListener('click', openForm);
+closePopupButton.addEventListener('click', closeForm);
+profileEditForm.addEventListener('submit', saveForm);
+postAddForm.addEventListener('submit', saveForm);
 
-console.log(editProfileButton);
