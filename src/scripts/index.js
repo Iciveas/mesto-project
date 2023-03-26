7c91fec3dde0a7c4e7a383511a7ad30dd91a8195
iciveas <zapahvesny@yandex.ru>
2023-03-26 15:23:52 +0400

    createpost

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ const nameInput = document.querySelector('.form__input_user-name');
 const signatureInput = document.querySelector('.form__input_user-signature');
 
 //Создаем переменные для работы с новым постом
+const postsGallery = document.querySelector('.posts-gallery');
 const postTemplate = document.querySelector('#post-template').content;
 
 //Получаем поп-ап и все формы в нём
@@ -21,6 +22,35 @@ const addPostButton = document.querySelector('.user-panel__add-post-button');
 //Получаем кнопки, универсальные для форм
 const closePopupButton = popup.querySelector('.popup__close-button'); //кнопка закрытия формы
 const saveButton = document.querySelector('.form__save-button'); //кнопка 'отправки' данных из формы
+
+//Создаем дефолтный массив для 6 постов
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 
 
 function openForm (evt) {
@@ -57,7 +87,14 @@ function saveForm(evt) {
   evt.preventDefault();
 
   if (evt.target.name === 'add-post') {
-    createPost();
+    //Создаем переменные для полей формы с будущим постом
+    const name = document.querySelector('.form__input_post-heading').value;
+    const link = document.querySelector('.form__input_post-picture-link').value;
+
+    createPost(name, link);
+
+    document.querySelector('.form__input_post-heading').value = ''
+    document.querySelector('.form__input_post-picture-link').value = '';
   }
   else {
     profileName.textContent = nameInput.value;
@@ -65,6 +102,25 @@ function saveForm(evt) {
   }
   closeForm();
 }
+
+function createPost (name, link) {
+  const postElement = postTemplate.querySelector('.posts-gallery__item').cloneNode(true);
+
+  postElement.querySelector('.posts-gallery__item-photo').setAttribute('src', link);
+  postElement.querySelector('.posts-gallery__item-heading').textContent = name;
+
+  console.dir(postElement.querySelector('.posts-gallery__item-heading').attributes);
+  postsGallery.append(postElement);
+  console.dir(postElement);
+}
+
+function createSomeTemplatePosts (postsArray) {
+  for (let i = 0; i < postsArray.length; i++) {
+    createPost(postsArray[i].name, postsArray[i].link);
+  }
+}
+
+createSomeTemplatePosts(initialCards);
 
 editProfileButton.addEventListener('click', openForm);
 addPostButton.addEventListener('click', openForm);
